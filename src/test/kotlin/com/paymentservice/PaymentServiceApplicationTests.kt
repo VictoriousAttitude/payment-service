f6277fea9d8f679d9a993ee5f475800bc4b4f0e9
VictoriousAttitude <xaverdima@gmail.com>
2026-05-07 10:12:41 +0000

    feat: initialize spring boot 3.5 project with kotlin and postgresql

---
+++ src/test/kotlin/com/paymentservice/PaymentServiceApplicationTests.kt
@@ -0,0 +1,15 @@
+package com.paymentservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+
+@Import(TestcontainersConfiguration::class)
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
